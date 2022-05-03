9d940202c57d485b80c6a43f617958667263f2e9
Jose Pereira suarez <johndoe@example.com>
2022-05-03 16:25:51 +0200

    [WEB MAD JC Y GUIILLE]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import contactsFromJson from "./contacts.json";
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [contacts, setContactsList] = useState(contactsFromJson)
+
+  let firstFive = contacts.slice(10, 15)
+
+  let removeContact = contactID => {
+    const newContacts = contacts.filter(elm => elm.id != contactID)              // retorna nuevo array
+    setContactsList(newContacts)
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Picture:</th>
+          <th>Name:</th>
+          <th>Popularity:</th>
+          <th>Won an Oscar:</th>
+          <th>Won an Emmy:</th>
+        </tr>
+
+        {
+          firstFive.map(contact => {
+            return (
+              <tr>
+
+                <td>{contact.name}</td>
+                <td><img src={contact.pictureUrl}></img></td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar ? <p> 🏆  </p> : <p>No tuvo Oscar</p>}</td>
+                <td>{contact.wonEmmy ? <p> 🏆  </p> : <p>No tuvo Emmy</p>}</td>
+                <td><button className="btn-delete" onClick={() => removeContact(contact.id)
+                }>Delete</button></td>
+
+              </tr>
+            )
+          })
+        }
+      </table>
+
+
+
     </div>
-  );
+  )
 }
 
 export default App;
